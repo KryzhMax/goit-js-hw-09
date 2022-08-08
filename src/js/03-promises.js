e0fb7b91deaf6534b4bd067a70a3a6684b328030
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-08 17:48:30 +0300

    3rd task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// References
+
+const refs = {
+  formRef: document.querySelector('.form'),
+  delayRef: document.querySelector('[name="delay"]'),
+  stepRef: document.querySelector('[name="step"]'),
+  amountRef: document.querySelector('[name="amount"]'),
+};
+
+const { formRef, delayRef, stepRef, amountRef } = refs;
+
+// Functions
+function addAmount(event) {
+  event.preventDefault();
+
+  let DELAY = delayRef.value;
+  let STEP = stepRef.value;
+  for (let i = 0; i < amountRef.value; i++) {
+    createPromise(i, DELAY)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// Listeners
+
+formRef.addEventListener('submit', addAmount);
